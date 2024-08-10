8d8e18622e9c7a73d5d2823a75b7177276ee9cc5
kherldhussein <eternety11@gmail.com>
2024-08-10 14:49:53 +0300

    Feat(ui): Implement basic CLI; Implement Player Action get function; this randomly assign stratefy if not or wrongly selected. Add simple way to display the outcome. Implement Final results view if there are more than one rounds Implement player set to dynamically pick the player names rather than hardcoding Implement playround to have more than one game this help to have multiple or continueous play rounds

--- cmd/main.go
+++ cmd/main.go
@@ -5,21 +5,45 @@ import (
 
 	"github.com/kh3rld/prisoners-dilemma/pkg/game"
 	"github.com/kh3rld/prisoners-dilemma/pkg/player"
+	"github.com/kh3rld/prisoners-dilemma/pkg/ui"
 )
 
+func setPlayers() (player.Player, player.Player) {
+	var player1Name, player2Name string
+
+	fmt.Print("Enter the name of Player 1: ")
+	fmt.Scanln(&player1Name)
+
+	fmt.Print("Enter the name of Player 2: ")
+	fmt.Scanln(&player2Name)
+
+	player1 := player.Player{Name: player1Name}
+	player2 := player.Player{Name: player2Name}
+
+	return player1, player2
+}
+
+func playRound(round int, player1, player2 *player.Player) {
+	fmt.Printf("Round %d\n", round)
+
+	player1.SetAction(ui.GetPlayerAction(player1.Name))
+	player2.SetAction(ui.GetPlayerAction(player2.Name))
+
+	g := game.Game{Player1: *player1, Player2: *player2}
+
+	g.DetermineOutcome()
+
+	ui.DisplayOutcome(g.Player1, g.Player2, g)
+}
+
 func main() {
-	player1 := player.Player{Name: "Kherld"}
-	player2 := player.Player{Name: "Ilara"}
+	player1, player2 := setPlayers()
 
-	game := game.Game{Player1: player1, Player2: player2}
+	rounds := ui.GetRounds()
 
-	game.Player1.SetAction("cooperate")
-	game.Player2.SetAction("defect")
+	for i := 1; i < rounds; i++ {
+		playRound(i, &player1, &player2)
+	}
 
-	game.DetermineOutcome()
-
-	outcome := game.Result
-	fmt.Println(outcome.Description)
-	fmt.Printf("%s gets %d years in prison.\n", game.Player1.Name, outcome.Player1)
-	fmt.Printf("%s gets %d years in prison.\n", game.Player2.Name, outcome.Player2)
+	ui.DisplayFinalResults(player1, player2)
 }
