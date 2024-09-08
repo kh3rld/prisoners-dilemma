eab409c6fc702952ef6b69e30465bd41aa55cbde
kherld-hussein <eternety11@gmail.com>
2024-09-09 06:38:20 +0900

    Refactor(ui): Update UI functions for better readability, error handling, and modularity

--- pkg/ui/ui.go
+++ pkg/ui/ui.go
@@ -48,15 +48,15 @@ func GetPlayerAction(name string) string {
 	}
 }
 
-func DisplayFinalResults(player1 player.Player, player2 player.Player) {
+func DisplayFinalResults(p1 player.Player, p2 player.Player) {
 	fmt.Println("Game Over!")
-	fmt.Printf("Total years in prison for %s: %d\n", player1.Name, player1.TotalYears)
-	fmt.Printf("Total years in prison for %s: %d\n", player2.Name, player2.TotalYears)
+	fmt.Printf("Total years in prison for %s: %d\n", p1.Name, p1.TotalYears)
+	fmt.Printf("Total years in prison for %s: %d\n", p2.Name, p2.TotalYears)
 
-	if player1.TotalYears < player2.TotalYears {
-		fmt.Printf("%s wins by cooperating more!\n", player1.Name)
-	} else if player1.TotalYears > player2.TotalYears {
-		fmt.Printf("%s wins by cooperating more!\n", player2.Name)
+	if p1.TotalYears < p2.TotalYears {
+		fmt.Printf("%s wins by cooperating more!\n", p1.Name)
+	} else if p1.TotalYears > p2.TotalYears {
+		fmt.Printf("%s wins by cooperating more!\n", p2.Name)
 	} else {
 		fmt.Println("It's a tie! Both players served the same amount of time.")
 	}
@@ -72,19 +72,19 @@ func DisplayGameStartMessage() {
 	fmt.Println("Special thanks to the Go community and open source contributors.")
 }
 
-func DisplayOutcome(player1 player.Player, player2 player.Player, outcome game.Game) {
-	fmt.Println(CenterText(fmt.Sprintf("%s chose to %s.", player1.Name, player1.Action)))
-	fmt.Println(CenterText(fmt.Sprintf("%s chose to %s.", player2.Name, player2.Action)))
-	fmt.Println(CenterText(outcome.Result.Description))
-	fmt.Println(CenterText(fmt.Sprintf("%s gets %d years in prison.", player1.Name, outcome.Result.Player1)))
-	fmt.Println(CenterText(fmt.Sprintf("%s gets %d years in prison.", player2.Name, outcome.Result.Player2)))
+func DisplayOutcome(p1 player.Player, p2 player.Player, game game.Game) {
+	fmt.Println(CenterText(fmt.Sprintf("%s chose to %s.", p1.Name, p1.Action)))
+	fmt.Println(CenterText(fmt.Sprintf("%s chose to %s.", p2.Name, p2.Action)))
+	fmt.Println(CenterText(game.Result.Description))
+	fmt.Println(CenterText(fmt.Sprintf("%s gets %d years in prison.", p1.Name, game.Result.Player1)))
+	fmt.Println(CenterText(fmt.Sprintf("%s gets %d years in prison.", p2.Name, game.Result.Player2)))
 	fmt.Println(CenterText("-----------------------------"))
 }
 
-func DisplayRoundSummary(round int, player1, player2 player.Player, outcome game.Game) {
+func DisplayRoundSummary(round int, p1, p2 player.Player, outcome game.Game) {
 	fmt.Printf("Round %d Summary:\n", round)
-	fmt.Printf("%s chose %s, %s chose %s\n", player1.Name, player1.Action, player2.Name, player2.Action)
-	fmt.Printf("Outcome: %s gets %d year%s, %s gets %d year%s\n", player1.Name, outcome.Result.Player1, pluralize(outcome.Result.Player1), player2.Name, outcome.Result.Player2, pluralize(outcome.Result.Player2))
+	fmt.Printf("%s chose %s, %s chose %s\n", p1.Name, p1.Action, p2.Name, p2.Action)
+	fmt.Printf("Outcome: %s gets %d year%s, %s gets %d year%s\n", p1.Name, outcome.Result.Player1, pluralize(outcome.Result.Player1), p2.Name, outcome.Result.Player2, pluralize(outcome.Result.Player2))
 }
 
 func pluralize(years int) string {
