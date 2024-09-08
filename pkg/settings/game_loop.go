f49a4072fe28808320f39ffe1e4656f2ce8b1c73
kherld-hussein <eternety11@gmail.com>
2024-09-09 04:10:51 +0900

    Refactor: changed the logic for checking empty player names and assigning them. Combined the logic in one step instead of asking for names if they are empty, followed by re-assigning them later

--- pkg/settings/game_loop.go
+++ pkg/settings/game_loop.go
@@ -5,28 +5,19 @@ import (
 
 	"github.com/kh3rld/prisoners-dilemma/pkg/common"
 	"github.com/kh3rld/prisoners-dilemma/pkg/game"
-	"github.com/kh3rld/prisoners-dilemma/pkg/network"
 	"github.com/kh3rld/prisoners-dilemma/pkg/player"
 	"github.com/kh3rld/prisoners-dilemma/pkg/ui"
 )
 
-var player1Name, player2Name string
-
 func SetPlayers() (common.PlayerInterface, common.PlayerInterface) {
-	p1 := &player.Player{Name: player1Name}
-	p2 := &player.Player{Name: player2Name}
+	p1 := &player.Player{}
+	p2 := &player.Player{}
 
-	if player1Name == "" {
-		fmt.Print("Enter the name of Player 1: ")
-		fmt.Scanln(&player1Name)
-		p1.SetName(player1Name)
-	}
+	fmt.Print("Enter the name of Player 1: ")
+	fmt.Scanln(&p1.Name)
 
-	if player2Name == "" {
-		fmt.Print("Enter the name of Player 2: ")
-		fmt.Scanln(&player2Name)
-		p2.SetName(player2Name)
-	}
+	fmt.Print("Enter the name of Player 2: ")
+	fmt.Scanln(&p2.Name)
 
 	return p1, p2
 }
@@ -56,21 +47,14 @@ func GameLoop(conn interface{}, player1, player2 *player.Player, rounds int, det
 		Rounds:  rounds,
 	}
 
-	if server, ok := conn.(*network.Server); ok {
-		game.Server = server
-		game.StartGameServer(detailedSummaries)
-	} else if client, ok := conn.(*network.Client); ok {
-		game.StartGameClient(client, detailedSummaries)
-	} else {
-		fmt.Println("Starting a local game...")
-		for i := 1; i <= rounds; i++ {
-			fmt.Printf("\nStarting Round %d\n", i)
-			player1.SetAction(ui.GetPlayerAction(player1.GetName()))
-			player2.SetAction(ui.GetPlayerAction(player2.GetName()))
-			game.DetermineOutcome()
-			ui.DisplayOutcome(*player1, *player2, *game)
-			ui.DisplayRoundSummary(i, *player1, *player2, *game)
-		}
-		return
+	fmt.Println("Starting a local game...")
+	for i := 1; i <= rounds; i++ {
+		fmt.Printf("\nStarting Round %d\n", i)
+		player1.SetAction(ui.GetPlayerAction(player1.GetName()))
+		player2.SetAction(ui.GetPlayerAction(player2.GetName()))
+		game.DetermineOutcome()
+		ui.DisplayOutcome(*player1, *player2, *game)
+		ui.DisplayRoundSummary(i, *player1, *player2, *game)
 	}
+
 }
