b5ea48501067a90218887d854a575e5eb3bf234b
kherld-hussein <eternety11@gmail.com>
2024-09-14 18:54:06 +0900

    Feat(net)

--- pkg/network/discovery.go
+++ pkg/network/discovery.go
@@ -3,21 +3,24 @@ package network
 import (
 	"fmt"
 	"net"
+	"sync"
 	"time"
 )
 
 const (
-	discoveryPort = ":9999" // Port for discovery messages
+	discoveryPort = ":9999"
 	broadcastMsg  = "DISCOVERY_REQUEST"
 	responseMsg   = "DISCOVERY_RESPONSE"
 )
 
 type DiscoveryService struct {
 	conn  *net.UDPConn
+	cm    *ConnectionManager
+	mu    sync.Mutex
 	peers []string
 }
 
-func NewDiscoveryService() (*DiscoveryService, error) {
+func NewDiscoveryService(cm *ConnectionManager) (*DiscoveryService, error) {
 	addr, err := net.ResolveUDPAddr("udp", discoveryPort)
 	if err != nil {
 		return nil, err
@@ -28,9 +31,14 @@ func NewDiscoveryService() (*DiscoveryService, error) {
 		return nil, err
 	}
 
-	return &DiscoveryService{conn: conn, peers: make([]string, 0)}, nil
+	return &DiscoveryService{
+		conn:  conn,
+		cm:    cm,
+		peers: make([]string, 0),
+	}, nil
 }
 
+// sends discovery requests periodically.
 func (ds *DiscoveryService) BroadcastDiscovery() {
 	addr, _ := net.ResolveUDPAddr("udp", discoveryPort)
 	for {
@@ -42,6 +50,7 @@ func (ds *DiscoveryService) BroadcastDiscovery() {
 	}
 }
 
+// listens for discovery responses and stores peer addresses.
 func (ds *DiscoveryService) ListenForPeers() {
 	buffer := make([]byte, 1024)
 	for {
@@ -54,11 +63,50 @@ func (ds *DiscoveryService) ListenForPeers() {
 		msg := string(buffer[:n])
 		if msg == responseMsg {
 			fmt.Printf("Received response from %s\n", addr.String())
+			ds.mu.Lock()
 			ds.peers = append(ds.peers, addr.String())
+			ds.mu.Unlock()
 		}
 	}
 }
 
+// returns a slice of discovered peers.
 func (ds *DiscoveryService) GetDiscoveredPeers() []string {
+	ds.mu.Lock()
+	defer ds.mu.Unlock()
 	return ds.peers
+}
+
+// starts the discovery and connection process
+func (ds *DiscoveryService) DiscoverAndConnect() {
+	go ds.BroadcastDiscovery()
+	go ds.ListenForPeers()
+
+	time.Sleep(10 * time.Second) // Wait for discovery
+
+	peers := ds.GetDiscoveredPeers()
+	fmt.Printf("Discovered peers: %v\n", peers)
+
+	// Initiate TCP connections to discovered peers
+	for _, peer := range peers {
+		go ds.connectToPeer(peer)
+	}
+}
+
+func (ds *DiscoveryService) connectToPeer(peer string) {
+	addr, err := net.ResolveTCPAddr("tcp", peer)
+	if err != nil {
+		fmt.Println("Error resolving peer address:", err)
+		return
+	}
+
+	conn, err := net.DialTCP("tcp", nil, addr)
+	if err != nil {
+		fmt.Println("Error connecting to peer:", err)
+		return
+	}
+
+	// Add connection to ConnectionManager
+	ds.cm.AddConnection(peer, conn)
+	fmt.Printf("Successfully connected to %s\n", peer)
 }
